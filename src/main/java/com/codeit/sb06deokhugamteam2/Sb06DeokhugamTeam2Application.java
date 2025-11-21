f430eff89166cee30418cd53e846eab88d0ec176
LeeJinWoo <jionu102@naver.com>
2025-11-21 17:52:55 +0900

    initialize project

---
+++ src/main/java/com/codeit/sb06deokhugamteam2/Sb06DeokhugamTeam2Application.java
@@ -0,0 +1,13 @@
+package com.codeit.sb06deokhugamteam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb06DeokhugamTeam2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb06DeokhugamTeam2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeit/sb06deokhugamteam2/Sb06DeokhugamTeam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.sb06deokhugamteam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb06DeokhugamTeam2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
